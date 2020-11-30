b2551d9151390f91bb0dd17200c172abcc90350c
Zixin Zeng <38689439+beestaicu@users.noreply.github.com>
2020-11-30 23:37:46 +0000

    Add files via upload

---
+++ GccApplication2/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 16/06/2020 09:19:14
+ * Author : bee
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
